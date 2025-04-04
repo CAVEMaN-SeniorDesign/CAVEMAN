f2ba29577c39ad54790596b6bbd1f0388637dfce
DZZLab <david.zhang@pitt.edu>
2025-04-04 18:01:44 +0000

    Modifications to Nav2 params

--- rover/launch/nav2.launch.py
+++ rover/launch/nav2.launch.py
@@ -1,25 +1,40 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
 
 import os
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
-    ekf_params = os.path.join(get_package_share_directory("rover"), 'config', 'nav2_params.yaml')
+    autostart = LaunchConfiguration('autostart')
 
-    Nav2 = Node(
-        package='nav2_bringup',
-        executable='bringup_launch.py',
-        output='screen',
-        parameters=[nav2_params],
-        arguments=['--ros-args', '--params-file', nav2_params]
+    nav2_params = os.path.join(
+        get_package_share_directory("rover"),
+        'config',
+        'nav2_params.yaml'
+    )
+
+    nav2_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory("nav2_bringup"),
+                'launch',
+                'navigation_launch.py'
+            )
+        ]),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+            'autostart': autostart,
+            'params_file': nav2_params
+        }.items()
     )
 
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='false'),
         DeclareLaunchArgument('autostart', default_value='true'),
-        Nav2
+        nav2_bringup_launch
     ])
